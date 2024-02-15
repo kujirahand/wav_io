5c6792c9c2d6334948172ea485ca78c5e61c1220
jaschutte <34577095+jaschutte@users.noreply.github.com>
2024-02-15 20:42:57 +0100

    Reverts LIST chunk
    This implementation is faulty as it assumes the LIST chunk comes before the `fmt ` chunk.
    Although this *may* be the case, it most commonly isn't, it instead happens *after* the `fmt ` chunk.

--- src/reader.rs
+++ src/reader.rs
@@ -80,17 +80,10 @@ impl Reader {
         if  wave_tag != "WAVE" {
             return Err(ERR_INVALID_FORMAT);
         }
-        // fmt (skip LIST chunk)
-        loop {
-            let chunk_tag = self.read_str4();
-            if chunk_tag == "" { // broken file | did not find fmt
-                return Err(ERR_BROKEN_WAV);
-            }
-            // println!("[info] chunk_tag={}", chunk_tag);
-            if chunk_tag == "fmt " { break; }
-            // skip LIST chunk
-            let chunk_size = self.read_u32().unwrap_or(0);
-            self.cur.set_position(self.cur.position() + chunk_size as u64);
+        // fmt
+        let fmt_tag = self.read_str4();
+        if fmt_tag != "fmt " {
+            return Err(ERR_INVALID_FORMAT);
         }
         let chunk_size = self.read_u32().unwrap_or(0);
         // audio format
